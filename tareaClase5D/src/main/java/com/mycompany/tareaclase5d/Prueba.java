c70792f273159d9f5aa2a39b406fe78c56767ee4
Maxi <mhof0902@g.educaand.es>
2022-02-23 14:44:42 +0100

    23 febrero 14:44

---
+++ tareaClase5D/src/main/java/com/mycompany/tareaclase5d/Prueba.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.tareaclase5d;
+
+/*Crea una clase de prueba para tu clase Ejército, donde se hagan uso de los métodos anteriores.
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
