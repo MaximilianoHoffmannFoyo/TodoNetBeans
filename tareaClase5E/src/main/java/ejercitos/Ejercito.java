a48015851501e19e05ebcb2da5b3f77d3a0fc8bd
MaximilianoHoffmannFoyo <maxihoffmannfoyo@gmail.com>
2022-02-27 17:37:20 +0100

    27 febrero 17:37

---
+++ tareaClase5E/src/main/java/ejercitos/Ejercito.java
@@ -0,0 +1,51 @@
+
+package ejercitos;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+Aplica lo estudiado para realizar una nueva clase llamada Ejercito, 
+que contenga un número de soldados N. El constructor debe inicializar 
+la estructura SET (no hay soldados duplicados y el orden no importa). 
+Debe haber métodos para:
+
+1. Saber el número de soldados que hay.
+2. Alistar a un solado.
+3. Saber si el ejército no tiene soldados.
+4. Saber si un soldado está en el ejército.
+4. Sacar todos los soldados, en forma de ArrayList.
+5. Desmatricular a un soldado del ejército.
+ */
+public class Ejercito {
+    
+    private Set<Soldado> listasoldados;
+
+    public Ejercito() {
+        this.listasoldados = new HashSet<>();
+    }
+    
+    public int numeroSoldados(){
+        return listasoldados.size();
+    }
+    public void alistarSoldado(Soldado s){
+        listasoldados.add(s);
+    }
+    public boolean tieneSoldados(){
+        return !listasoldados.isEmpty();
+    }
+    public boolean siSoldadoEnEjercito(Soldado s){
+        return listasoldados.contains(s);
+    }
+    public ArrayList devolverListaEnArrayList(){
+        ArrayList<Soldado> array = new ArrayList<>();
+        for (Soldado s : listasoldados) {
+            array.add(s);
+        }
+        return array; 
+    }    
+    public void desmatricularSoldado(Soldado s){
+        listasoldados.remove(s);
+    }
+}
