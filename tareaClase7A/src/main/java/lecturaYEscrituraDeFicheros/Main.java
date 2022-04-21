a35be0296aad1b2c14da0ac70d38ba22bdf73df3
MaximilianoHoffmannFoyo <maxihoffmannfoyo@gmail.com>
2022-04-21 21:41:33 +0200

    me quiero morir

--- tareaClase7A/src/main/java/lecturaYEscrituraDeFicheros/Main.java
+++ tareaClase7A/src/main/java/lecturaYEscrituraDeFicheros/Main.java
@@ -4,7 +4,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Main {
 
@@ -22,7 +24,7 @@ public class Main {
         // Inicialización del flujo "datosFichero" en función del archivo llamado "idFichero"
         // Estructura try-with-resources. Permite cerrar los recursos una vez finalizadas
         // las operaciones con el archivo
-        try ( Scanner datosFichero = new Scanner(new File(idFichero), "ISO-8859-1")) {
+        try (Scanner datosFichero = new Scanner(new File(idFichero), "ISO-8859-1")) {
             datosFichero.nextLine();
             // hasNextLine devuelve true mientras haya líneas por leer
             while (datosFichero.hasNextLine()) {
@@ -35,33 +37,46 @@ public class Main {
                 tokens[0] = tokens[0].substring(1);
                 tokens[7] = tokens[7].substring(0, tokens[7].length() - 1);
                 for (String token : tokens) {
-                    System.out.print(token + ";");
+//                    System.out.print(token + ";");
                 }
-                    lista.add(new Empleado(tokens[0], tokens[1], tokens[2],
-                            Main.convertirEnLocalDate(tokens[3]), Main.convertirEnLocalDate(tokens[4]),
-                            tokens[5], tokens[6], tokens[7]));
-                
+                lista.add(new Empleado(tokens[0], tokens[1], tokens[2],
+                        Main.convertirEnLocalDate(tokens[3]), Main.convertirEnLocalDate((tokens[4])),
+                        tokens[5], tokens[6], tokens[7]));
 //                System.out.println("");
-//                for (Empleado v : lista) {
-//                    System.out.println(v.toString());
-//                }
             }
+
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
-//        Main.convertirEnLocalDate("3/3/1999");
+
+        //MOSTRAR DATOS
+            //Lista optenida del fichero
+        for (Empleado v : lista) {
+            System.out.println(v.toString());
+        }
+            //Total de profesores
+        System.out.println("El total de profesores se de: "+lista.size());
+        
 
     }
 
     public static LocalDate convertirEnLocalDate(String fechaString) {
-        if(fechaString == null){
-        String[] fechaArray = fechaString.split("/");
         LocalDate fecha;
-        fecha = LocalDate.of(Integer.parseInt(fechaArray[2]),
-                Integer.parseInt(fechaArray[1]), Integer.parseInt(fechaArray[0]));
+        if (!fechaString.isBlank()) {
+            String[] fechaArray = fechaString.split("/");
+            fecha = LocalDate.of(Integer.parseInt(fechaArray[2]),
+                    Integer.parseInt(fechaArray[1]), Integer.parseInt(fechaArray[0]));
+        } else {
+            fecha = null;
         }
 //        System.out.println(fecha);
         return fecha;
     }
 
+    public static void map(ArrayList<Empleado> l){
+        Map<String, Empleado> mapa = new TreeMap<>();
+//        for (Empleado e : l) {
+//            mapa.put(e.getPuesto(), value);            
+//        }
+    }
 }
