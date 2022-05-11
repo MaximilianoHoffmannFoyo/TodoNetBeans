fc8b3369b7280e45d3dd5c3867f006388a2906f8
Maxi <mhof0902@g.educaand.es>
2022-05-11 14:09:33 +0200

    11 mayo 14:09

---
+++ backendjavaJson/src/main/java/jcarlos/backendjavajson/Company.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jcarlos.backendjavajson;
+
+/**
+ *
+ * @author maxi
+ */
+public class Company{
+    private String name;
+    private String catchPhrase;
+    private String bs;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCatchPhrase() {
+        return catchPhrase;
+    }
+
+    public void setCatchPhrase(String catchPhrase) {
+        this.catchPhrase = catchPhrase;
+    }
+
+    public String getBs() {
+        return bs;
+    }
+
+    public void setBs(String bs) {
+        this.bs = bs;
+    }
+    
+    
+}
---
+++ backendjavaJson/src/main/java/jcarlos/backendjavajson/Geo.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jcarlos.backendjavajson;
+
+/**
+ *
+ * @author maxi
+ */
+public class Geo{
+    private String lat;
+    private String lng;
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+    
+    
+}
