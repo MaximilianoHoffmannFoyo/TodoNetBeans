3bb31534b31d0fc793db182bb7306525edcea837
Maxi <mhof0902@g.educaand.es>
2022-05-18 13:24:33 +0200

    18 mayo 13:24

--- p81Maximiliano/src/main/java/aplicacion/Programa.java
+++ p81Maximiliano/src/main/java/aplicacion/Programa.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aplicacion;
 
 import modelo.AventuraDAO;
@@ -11,10 +6,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author J. Carlos F. Vico <jcarlosvico@maralboran.es>
- */
 public class Programa {
 
     public static void main(String[] args) {
